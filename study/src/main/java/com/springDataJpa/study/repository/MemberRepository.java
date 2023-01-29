f10aef49085566defa87173e1843b11f00473e32
sangdob <51118110+sangdob@users.noreply.github.com>
2023-01-29 23:46:57 +0900

    spring data jpa
    namedQuery Repository add

--- study/src/main/java/com/springDataJpa/study/repository/MemberRepository.java
+++ study/src/main/java/com/springDataJpa/study/repository/MemberRepository.java
@@ -15,6 +15,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findTop3HelloBy();
 
-    @Query(name = "Member.findByUsername")
+    /**
+     * query 필수사항이 아님 namedQuery를 우선순위로 찾아 적용하도록 만들어 져 있음
+     * @param username
+     * @return
+     */
+//    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
