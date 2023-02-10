5504da0ff143e0fe3801a223422995574b5bd91c
sangdob <51118110+sangdob@users.noreply.github.com>
2023-02-10 21:03:19 +0900

    spring data jpa
    spring data jpa test
    Auditing enable entity

--- study/src/main/java/com/springDataJpa/study/entity/BaseEntity.java
+++ study/src/main/java/com/springDataJpa/study/entity/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.springDataJpa.study.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -21,4 +23,11 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
 }
