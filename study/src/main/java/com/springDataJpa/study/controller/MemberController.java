f461e7352f69633b2658db99896a19e3403f36c2
sangdob <51118110+sangdob@users.noreply.github.com>
2023-02-12 23:13:58 +0900

    spring data jpa
    web example
    page
    index setting and maxPageSize settings

--- study/src/main/java/com/springDataJpa/study/controller/MemberController.java
+++ study/src/main/java/com/springDataJpa/study/controller/MemberController.java
@@ -3,6 +3,9 @@ package com.springDataJpa.study.controller;
 import com.springDataJpa.study.entity.Member;
 import com.springDataJpa.study.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +20,10 @@ public class MemberController {
     private final MemberRepository memberRepository;
 
     @PostConstruct
-    public void init(){
-        memberRepository.save(new Member("userA"));
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
     @GetMapping("/members/{id}")
@@ -30,8 +35,11 @@ public class MemberController {
 
     @GetMapping("/members2/{id}")
     public ResponseEntity findMember2(@PathVariable("id") Member member) {
-
         return ResponseEntity.ok().body(member.getUsername());
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
 }
