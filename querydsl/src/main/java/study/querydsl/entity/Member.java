903829f09fa7cc2f3212895bb84a77a83d9f6252
sangdob <51118110+sangdob@users.noreply.github.com>
2023-02-20 01:27:59 +0900

    - querydsl study start -
    start test

---
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,20 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue
+    private Long id;
+}
