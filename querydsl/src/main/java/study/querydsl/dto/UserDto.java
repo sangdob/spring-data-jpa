8ac46898ef1fffda737afd0fe80e16c9aab5f05d
sangdob <1683229a@>
2023-03-03 23:28:00 +0900

    - querydsl study start -
    userDto constructor add

--- querydsl/src/main/java/study/querydsl/dto/UserDto.java
+++ querydsl/src/main/java/study/querydsl/dto/UserDto.java
@@ -10,4 +10,9 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private String name;
     private int age;
+
+    public UserDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
